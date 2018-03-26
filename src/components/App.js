4ead8cc32a296cbae37d2459381f750a4176eea6
dragonman117 <tj.ferrell@gmail.com>
2018-03-26 14:13:55 -0600

    Added intial navigator code

--- src/components/App.js
+++ src/components/App.js
@@ -12,55 +12,58 @@ import {
   View
 } from 'react-native';
 import Level from '../store/levels';
+import { StackNavigator } from 'react-navigation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+// type Props = {};
+// export default class App extends Component<Props> {
+//
+//   constructor(){
+//     super();
+//     levelGen = new Level();
+//     console.log(levelGen.genLevel());
+//   }
+//
+//   //screenPrompt
+//
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>HI</Text>
+//       </View>
+//     );
+//   }
+// }
 
-  constructor(){
-    super();
-    levelGen = new Level();
-    console.log(levelGen.genLevel());
-  }
+class HomeScreen extends React.Component {
+    static navigationOptions = {
+        header: null
+        /* No more header config here! */
+    };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Home Screen</Text>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+export default StackNavigator({
+    Home: {
+        screen: HomeScreen,
+        title: "Home"
+    },
+}, {
+  initialRoute: 'Home',
+  navigationOptions: {
+      headerStyle: {
+          backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+          fontWeight: 'bold',
+      },
   },
 });
