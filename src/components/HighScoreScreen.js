805f3b67959144f4b163ca2f7f6e51e24c0340ef
dragonman117 <tj.ferrell@gmail.com>
2018-03-28 13:39:13 -0600

    Merge branches 'backgroundBranchTJ' and 'master' of https://github.com/dragonman117/Mystery into backgroundBranchTJ
    # Conflicts:
    #	src/components/App.js
    Made ScreenPrompt the home page
    
    Added game screen and high score screen

---
+++ src/components/HighScoreScreen.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class HighScoreScreen extends Component<Props> {
+
+  constructor(){
+    super()
+  }
+
+  //screenPrompt
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>HI</Text>
+      </View>
+    );
+  }
+}
